d544462ee4571416d925dae4f170ced0732edd23
Julio Gimenez <julkleon3@gmail.com>
2020-06-19 22:23:59 -0400

    My first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,297 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link rel="icon" type="image/png" href="images/favicon-32x32.png" sizes="32x32" />
+  <link rel="icon" type="image/png" href="images/favicon-16x16.png" sizes="16x16" />
+  <title>Feliz Cumpleaños Romina!!!</title>
+  <!--stylesheet-->
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700,900" rel="stylesheet">
+  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+  <link href="styles/styles.css" rel="stylesheet" type="text/css">
+  <link href="styles/custom-responsive-styles.css" rel="stylesheet" type="text/css">
+  <!--scripts-->
+  <script type="text/javascript" src="scripts/jquery-3.2.1.min.js"></script>
+  <script type="text/javascript" src="scripts/all-plugins.js"></script>
+  <script type="text/javascript" src="scripts/plugins-activate.js"></script>
+</head>
+
+<body id="page-top">
+  <!-- Navigation -->
+  <div class="logo">
+    <i class="fa fa-plane" aria-hidden="true"><span> Ojalá pases bien hoy!!</span></i>
+  </div>
+  <a class="menu-toggle rounded" href="#">
+    <i class="fa fa-bars"></i>
+  </a>
+  <nav id="sidebar-wrapper">
+    <ul class="sidebar-nav">
+      <li class="sidebar-brand">
+        <a class="smooth-scroll" href="#Header"></a>
+      </li>
+      <li class="sidebar-nav-item">
+        <a class="smooth-scroll" href="#page-top">Home</a>
+      </li>
+      <li class="sidebar-nav-item">
+        <a class="smooth-scroll" href="#About">Things</a>
+      </li>
+      <li class="sidebar-nav-item">
+        <a class="smooth-scroll" href="#Services">Dreams</a>
+      </li>
+      <li class="sidebar-nav-item">
+        <a class="smooth-scroll" href="#Portfolio">Memories</a>
+      </li>
+      <li class="sidebar-nav-item">
+        <a class="smooth-scroll" href="#Testimonials">Anécdotas y algo más...</a>
+      </li>
+    </ul>
+  </nav>
+  <!-- Header Starts -->
+  <section id="Banner" class="content-section">
+    <div class="container content-wrap text-center">
+      <h1>Muchisimas Felicidades para vos en serio!!!</h1>
+      <h3>
+          <em>Gracias por abrir el link. Realmente no sabría si lo harias...</em>
+        </h3>
+      <a class="btn btn-primary btn-xl smooth-scroll" href="#About">Sigue hacia abajo...</a>
+    </div>
+    <div class="overlay"></div>
+  </section>
+  <!-- Header Ends -->
+  <!-- About Us Starts -->
+  <section id="About" class="content-section">
+    <div class="container text-center">
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="block-heading">
+            <h2>Things</h2>
+          </div>
+          <p class="lead">Se que realmente no deberia decir esto pero... realmente quiero decirlo, se que 
+          la cagué, sé que decirte esto es egoísta, capaz y demasiado egoísta pero bueno, desde ya lo siento...
+          Lo siento también por la redundancias que hay y habrán en los textos. Bueno demasiado rodeo, TE EXTRAÑO
+          y en serio te extraño demasiado... probablemente leer esto te va a molestar o capaz y ni te importe, una
+        posibilidad tambien es la verdad que ni lo estes leyendo pero bueno, como ya lo dije quiero hacerlo:</p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- About Us Starts -->
+  <section id="Services" class="content-section text-center">
+    <div class="container">
+      <div class="block-heading">
+        <h2>Dreams</h2>
+        <p>Ultimamente tengo un deseo fuerte de volver al pasado y sueño lo mismo todos los días, estar contigo, de
+        repente me vienen algunos momentos que pasamos juntos y la cagué por mi inmadurez, en serio reconozco que 
+      para ser una persona tan supuestamente "inteligente" era demasiado inmaduro e infantil, capaz no mejoré mucho la 
+      verdad entre antes y ahora pero realmente era hasta estúpido y me da tanta rabia que bueno, es dificil. P.D: Acabo 
+      de realizar una cagada, un impulso raro y te llamé, bueno ojalá no te haya molestado :c </p>
+      P.D 2: Pon el mouse encima de los siguientes cuadros pls.
+      </div>
+      <div class="row">
+        <div class="col-md-3 col-sm-6">
+          <div class="service-box">
+            <div class="service-icon yellow">
+              <div class="front-content">
+                <i class="fa fa-globe" aria-hidden="true"></i>
+                <h3>Lost on me</h3>
+              </div>
+            </div>
+            <div class="service-content">
+              <h3>Soy un estúpido</h3>
+              <p>Yo sé que ya te perdí, tenia a la mujer perfecta a mi lado y la cagué, tanta luz que emitias capaz
+               y me daba miedo, capaz y me ayudaría a salir de la oscuridad y no quería eso, cuando terminamos trate de 
+               hacer de todo para superarte, no te voy a mentir. </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3 col-sm-6">
+          <div class="service-box">
+            <div class="service-icon orange">
+              <div class="front-content">
+                <i class="fa fa-suitcase"></i>
+                <h3>I need something</h3>
+              </div>
+            </div>
+            <div class="service-content">
+              <h3> Need</h3>
+              <p> Me metí de lleno en la vida común de las personas, me metí de lleno al mismo tiempo en los videojuegos, si, más que antes, me metí de lleno en aprender informática, en estos pocos meses me llene de certificados y conocimientos y realmente gracias a todo eso que paso, maduré, afronté la realidad y me odié errores pasados.  </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3 col-sm-6">
+          <div class="service-box ">
+            <div class="service-icon red">
+              <div class="front-content">
+                <i class="fa fa-male" aria-hidden="true"></i>
+                <h3>I'm empty</h3>
+              </div>
+            </div>
+            <div class="service-content">
+              <h3>Empty inside</h3>
+              <p> Siempre que me pasaba algo, conseguía algo o habia algún acontecimiento raro me faltabas vos, alguien a quién contarle si tenia, pero no eras vos, se que es irónico ésto porque antes nunca queria contar nada pero bueno, son cosas de la vida lastimosamente, no salís nunca de mi cabeza, es una realidad indiscutible.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3 col-sm-6">
+          <div class="service-box">
+            <div class="service-icon grey">
+              <div class="front-content">
+                <i class="fa fa-users"></i>
+                <h3>When you get older, plainer, saner</h3>
+              </div>
+            </div>
+            <div class="service-content">
+              <h3>Stupid things</h3>
+              <p>Fui yo el que termino todo por no pensar claramente, por dejarme llevar por comentarios toxicos, por no hacer
+              caso a mi subconsciente, por tener miedo de la vida de adulto, por tener miedo a los cambios que puedan llegar, en sintesis por no querer afrontar la nueva realidad que estaba ahí.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="content-section text-center" id="Portfolio">
+    <div class="container">
+      <div class="block-heading">
+        <h2>Memories</h2>
+        <p>Recordar aquellos momentos en los que pasamos bien/mal, no importa, solo quiero volver a ellos y hacer las cosas
+        como realmente quiero </p>
+      </div>
+      <div class="portfolio-wrapper clearfix">
+        <a class="each-portfolio" data-fancybox="gallery" href="images/RxJ5.jpg">
+          <div class="content hover-cont-wrap">
+            <div class="content-overlay"></div>
+            <img class="content-image" src="images/RxJ5.jpg" width="100" height="100">
+            <div class="content-details fadeIn-bottom">
+              <h5 class="p-title">Revivir</h5>
+              <p class="p-desc">Revivir esos momentos en el que podia mirarte todo lo que quiera..</p>
+              <span class="zoom"><i class="fa fa-search-plus"></i></span>
+            </div>
+          </div>
+        </a>
+        <a class="each-portfolio" data-fancybox="gallery" href="images/RxJ6.jpg">
+          <div class="content hover-cont-wrap">
+            <div class="content-overlay"></div>
+            <img class="content-image" src="images/RxJ6.jpg">
+            <div class="content-details fadeIn-bottom">
+              <h5 class="p-title">Jamás</h5>
+              <p class="p-desc">Jamás voy a poder olvidar esa hermoosa carita de ángel que tenés :c</p>
+              <span class="zoom"><i class="fa fa-search-plus"></i></span>
+            </div>
+          </div>
+        </a>
+        <a class="each-portfolio" data-fancybox="gallery" href="images/RxJ3.jpg">
+          <div class="content hover-cont-wrap">
+            <div class="content-overlay"></div>
+            <img class="content-image" src="images/RxJ3.jpg">
+            <div class="content-details fadeIn-bottom">
+              <h5 class="p-title">Sonrisa</h5>
+              <p class="p-desc">Y esa sonrisa :c, por más que a veces podrían ser falsas, haría todo lo que fuera
+              para poder tener la oportunidad de hacerte reir o sonreír de nuevo </p>
+              <span class="zoom"><i class="fa fa-search-plus"></i></span>
+            </div>
+          </div>
+        </a>
+        <a class="each-portfolio" data-fancybox="gallery" href="images/RxJ4.jpg">
+          <div class="content hover-cont-wrap">
+            <div class="content-overlay"></div>
+            <img class="content-image" src="images/RxJ4.jpg">
+            <div class="content-details fadeIn-bottom">
+              <h5 class="p-title">Momentos</h5>
+              <p class="p-desc">Capaz no me haya portado de la mejor manera pero aún asi quiero volver a compartir 
+              contigo aquellos momentos únicos que van a quedar marcados en mi vida por siempre, aún quedan millones...</p>
+              <span class="zoom"><i class="fa fa-search-plus"></i></span>
+            </div>
+          </div>
+        </a>
+        <a class="each-portfolio" data-fancybox="gallery" href="images/RxJ1.jpg">
+          <div class="content hover-cont-wrap">
+            <div class="content-overlay"></div>
+            <img class="content-image" src="images/RxJ1.jpg">
+            <div class="content-details fadeIn-bottom">
+              <h5 class="p-title">Pasado</h5>
+              <p class="p-desc">Todo hubiese sido diferente si yo hubiese tenido tu nivel de madurez en aquel momento o si
+              me dejase llevar menos por la estúpida sociedad en aquel entonces, ser como soy ahora hubiese estado mucho mejor
+            ...</p>
+              <span class="zoom"><i class="fa fa-search-plus"></i></span>
+            </div>
+          </div>
+        </a>
+          </div>
+        </a>
+      </div>
+    </div>
+  </section>
+  <!-- TESTIMONIALS -->
+  <section id="Testimonials" class="content-section">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12">
+          <div class="block-heading">
+            <h2>Despedida y algo más...</h2>
+            <p>Ésta página debía de ser un regalo y no algo tan lamentable, se me fue de las manos el escribir todo esto
+            lo siento :c</p>
+          </div>
+          <div id="customers-testimonials" class="owl-carousel">
+            <div class="item">
+              <div class="shadow-effect">
+                <img class="img-circle" src="images/Heart.jpg" alt="">
+                <p>Maria Romina Abigail Quintana Moreno, Te Amo con todo mi ser, repetí esto seguramente varias veces
+                pero realmente sos El Amor de Mi Vida, nunca te voy a olvidar te lo prometo, nunca voy a conseguir otra como vos
+                , no quiero alguien como vos... te quiero a vos en mi vida... A nadie más.</p>
+              </div>
+              <div class="testimonial-name">I miss you my alien 1%</div>
+            </div>
+            <div class="item">
+              <div class="shadow-effect">
+                <img class="img-circle" src="images/thanks.jpg" alt="">
+                <p>Si conseguiste llegar hasta acá realmente lo aprecio demasiado, en uno de esos días en donde andaba desesperado
+                stalkeandote y demás se me ocurrió esta idea de poder decirte algunas cositas, pero capaz se me pasó de las manos. Haría este tipo de cosas y mucho más por vos.</p>
+              </div>
+              <div class="testimonial-name">Aunque no quiero molestarte.</div>
+            </div>
+            <div class="item">
+              <div class="shadow-effect">
+                <img class="img-circle" src="images/trouble.jpg" alt="">
+                <p>Aún hay tantas cosas que quiero decirte aún pero sería exagerado, soy egoísta, lo sé, en serio si pienso en lo que estoy haciendo capaz no sea lo correcto, pero ya está hecho. Lo único que no quiero es que ésto te haga algún tipo de daño. </p>
+              </div>
+              <div class="testimonial-name">Si así fue, en serio lo siento demasiado, no es mi intención.</div>
+            </div>
+            <div class="item">
+              <div class="shadow-effect">
+                <img class="img-circle" src="images/sky.jpg" alt="">
+                <p>Dios te bendiga y a tu familia, Dios quiera que pases bien hoy y que siga así por siempre. No soy el indicado pero estoy dispuesto a ganar tu confianza de nuevo, si necesitas alguien con quién hablar, daré lo mejor de mi para ser esa persona. Daría todo para volver a estar contigo.</p>
+              </div>
+              <div class="testimonial-name">Amy Tan</div>
+            </div>
+            <div class="item">
+              <div class="shadow-effect">
+                <img class="img-circle" src="images/firstlove.jpg" alt="">
+                <p> Muchas Gracias por leer todo lo que te escribí, como verás estaba desesperado y por eso redacté esto con unos fallos, traté de hacer lo mejor por vos sin parecer tan loco. Quiero que seas feliz, pase lo que pase sabes que puedes superarlo, soy testigo de que sos una mujer muy fuerte. Si estas con Dios superaras todo.</p>
+              </div>
+              <div class="testimonial-name"> I will miss you a lot my first love</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- END OF TESTIMONIALS -->
+  
+  <footer class="footer text-center">
+    <div class="container">
+      <ul class="list-inline">
+        <li class="list-inline-item">
+        </li>
+      </ul>
+      <p class="text-muted small mb-0">Aishiteru</p>
+      <p class="text-muted small mb-0">Saranghae</p>
+    </div>
+  </footer>
+</body>
+
+</html>
